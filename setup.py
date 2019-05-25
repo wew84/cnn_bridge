a1d9937dabe6f592b660610c76faab2ae4adde1b
Noam C. Golombek <ngolombek@gmail.com>
2019-05-25 15:01:43 +0300

    Python dependencies to setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
+    requires=['tensorflow', 'numpy', 'opencv']
     packages=['cnn_bridge_main'],
     package_dir={'': 'src'},
 )
